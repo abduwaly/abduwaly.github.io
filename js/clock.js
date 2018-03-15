3ea8c3f60bb75cb984d3e16c1f88fa9fede8930c
arsay <arsay@foxmail.com>
2018-03-15 11:55:28 +0800

    fix : fix layout bug & adapt mobile browser ;

--- js/clock.js
+++ js/clock.js
@@ -1,9 +1,9 @@
 var canvas = document.getElementById("clock-canvas");
 var ctx = canvas.getContext("2d");
-//中心点
+
 var cx = canvas.width/2;
 var cy = canvas.height/2;
-//把画布原点放在中间
+
 ctx.translate(cx,cy);
 
 drawWatch();
@@ -16,22 +16,22 @@ function drawWatch(){
     ctx.stroke();
     ctx.restore();
 
-    //刻度
+
     drawDial();
-    //数字
+
     drawNumber()
-    //针
+
     drawNeedle()
 }
 
-//绘制刻度
+
 function drawDial(){
     var angle = 0;
     for(var i = 0; i < 60; i++){
         ctx.save();
-        //旋转
+
         ctx.rotate(angle);
-        //开始绘制刻度
+
         ctx.beginPath();
         ctx.moveTo(109,0);
         ctx.lineTo(119,0)
@@ -39,16 +39,16 @@ function drawDial(){
         ctx.lineWidth = 2;
         ctx.stroke();
         ctx.restore();
-        //角度累加：转为弧度
+
         angle += Math.PI/180*6;
     }
 
     var angle = 0;
     for(var i = 0; i < 12; i++){
         ctx.save();
-        //旋转
+
         ctx.rotate(angle);
-        //开始绘制刻度
+
         ctx.beginPath();
         ctx.moveTo(105,0);
         ctx.lineTo(119,0)
@@ -56,12 +56,12 @@ function drawDial(){
         ctx.lineWidth = 5;
         ctx.stroke();
         ctx.restore();
-        //角度累加：转为弧度
+
         angle += (Math.PI/180)*30;
     }
 }
 
-//画数字
+
 function drawNumber(){
     var angle = 0;
     for(var i = 12; i > 0; i--){
@@ -69,9 +69,9 @@ function drawNumber(){
         ctx.rotate(-angle);
 
         ctx.translate(0,-97);
-        //把文字转回垂直显示
+
         ctx.rotate(angle);
-        //开始绘制文字
+
         ctx.font = "14px"
         ctx.textAlign = "center";
         ctx.textBaseline = "middle";
@@ -81,62 +81,62 @@ function drawNumber(){
     }
 }
 
-//针
+
 function drawNeedle(){
-    //获取最新时间
+
     currentTime();
-    //时针
+
     ctx.save();
-    //旋转操作在绘制的上面，save方法内部
+
     ctx.rotate(-Math.PI/2);
     var angle = ctx.hour * (Math.PI/180) * 30;
-    //1小时 == 60分 == 30度  一分钟0.5度
+
     var hAngle = ctx.minutes * (Math.PI/180) * 0.5;
-    //1小时 == 60分 == 3600秒 == 30度  一秒钟1/120度
+
     var sAngle = ctx.seconds * (Math.PI/180)* 1/120;
     ctx.rotate(angle + hAngle + sAngle);
     ctx.beginPath();
     ctx.strokeStyle = "#000";
-    ctx.lineCap = "round";//棱角处理
+    ctx.lineCap = "round";
     ctx.lineWidth = 8;
     ctx.moveTo(0,0);
     ctx.lineTo(50,0);
     ctx.stroke();
     ctx.restore();
 
-    //分针
+
     ctx.save();
     ctx.rotate(-Math.PI/2);
-    //每分钟6度
+
     var angle = ctx.minutes*(Math.PI/180)*6;
-    //1分钟 == 60秒 == 6度  每秒0.1度
+
     var sAngle = ctx.seconds*(Math.PI/180)*0.1;
     ctx.rotate(angle+sAngle);
     ctx.beginPath();
     ctx.strokeStyle = "red";
-    ctx.lineCap = "round";//棱角处理
+    ctx.lineCap = "round";
     ctx.lineWidth = 6;
     ctx.moveTo(0,0);
     ctx.lineTo(70,0);
     ctx.stroke();
     ctx.restore();
 
-    //秒针
+
     ctx.save();
     ctx.rotate(-Math.PI/2);
-    //每秒钟6度
+
     var angle = ctx.seconds*(Math.PI/180)*6;
     ctx.rotate(angle);
     ctx.beginPath();
     ctx.strokeStyle = "yellow";
-    ctx.lineCap = "round";//棱角处理
+    ctx.lineCap = "round";
     ctx.lineWidth = 4;
     ctx.moveTo(0,0);
     ctx.lineTo(90,0);
     ctx.stroke();
     ctx.restore();
 
-    //原点
+
     ctx.save();
     ctx.beginPath();
     ctx.fillStyle = "gray";
@@ -144,13 +144,13 @@ function drawNeedle(){
     ctx.fill();
     ctx.restore();
 
-    //数字时钟
+
     $("#digital-clock .hour").html(ctx.time.substr(0,2));
     $("#digital-clock .minute").html(ctx.time.substr(3,2));
     $("#digital-clock .second").html(ctx.time.substr(6,2));
 }
 
-//获取时间
+
 function currentTime(){
     var dateC = new Date();
     ctx.time = dateC.toTimeString().substr(0,9);
@@ -159,9 +159,8 @@ function currentTime(){
     ctx.seconds = dateC.getSeconds();
 }
 
-//定时器
+
 setInterval(function(){
-    //清除画布
     ctx.clearRect(-cx,-cy,canvas.width,canvas.height);
     drawWatch();
 },1000);
--- js/home.js
+++ js/home.js
@@ -40,12 +40,15 @@ var Home = {
 		// }
 		
 		return '\
-			<div class="blog-item">\
-				<h3 class="blog-title">'+ blog.title +'</h3>\
+			<div class="blog-item" data-blogId="'+ blog.id +'">\
+				<h3 class="blog-title"><a href="#">'+ blog.title +'</a></h3>\
 				<p class="blog-brief"> '+ blog.brief +' </p>\
 				<div class="blog-item-bottom">\
 					<div class="bottom-left publish-time">'+ blog.publish_time +'</div>\
 					<div class="bottom-right">\
+						<div class="blog-like">\
+							<div class="icon icon-like"></div><span>'+ blog.like +'</span>\
+						</div>\
 						<div class="blog-read-count">\
 							<div class="icon icon-read"></div><span>'+ blog.view +'</span>\
 						</div>\
